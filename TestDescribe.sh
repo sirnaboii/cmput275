cc3b8a8523ab69a3df5df9451553d40b01b56a05
sirnaboii <sirnaboii@gmail.com>
2026-02-05 21:36:23 -0700

    Add files via upload

--- TestDescribe.sh
+++ TestDescribe.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-echo "hello world!"
-echo "this is the first command line"; echo "this is the second command"
+
+if [ $$ -eq 0 ]; then
+    echo "Segmentation fault"
+    exit 1
+fi
+
+for i in $(cat $1); do
+    echo Description for test case ${i}:
+    var="i.desc"
+    if [ -f $location ]; then
+        echo ${cat $location}
+    else
+        echo ${i} No test description
+    fi
+done
